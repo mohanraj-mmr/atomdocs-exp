63ad8f81bbf3af0a072c434f3de40d4d3385665f
mohanraj-mmr <xeuswarlock@gmail.com>
2025-07-21 22:51:30 +0530

    Deleted next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // env: {
-  //   NEXT_PUBLIC_APP_STATE: 'edit',
-  //   NEXT_PUBLIC_APP_TITLE: 'Atom Docs'
-  // }
-};
-
-export default nextConfig;
